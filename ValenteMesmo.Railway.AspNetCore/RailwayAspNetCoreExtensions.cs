5881156bcdb10b55d9b5250468d6dba56468b2a3
ValenteMesmo <valentemesmo@gmail.com>
2021-09-23 14:02:02 -0300

    async extensions

--- ValenteMesmo.Railway.AspNetCore/RailwayAspNetCoreExtensions.cs
+++ ValenteMesmo.Railway.AspNetCore/RailwayAspNetCoreExtensions.cs
@@ -1,18 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Threading.Tasks;
 
-namespace ValenteMesmo.Railway
+namespace ValenteMesmo
 {
     public static class RailwayAspNetCoreExtensions
     {
-        public static IActionResult MaybeOk<T>(this Railway<T> value) =>
+        public static IActionResult Ok<T>(this Railway<T> value) =>
             HandleWithSuccessCode(200, value);
 
-        public static IActionResult MaybeCreated<T>(this Railway<T> value) =>
+        public static async Task<IActionResult> Ok<T>(this Task<Railway<T>> value) =>
+            HandleWithSuccessCode(200, await value);
+
+        public static IActionResult Created<T>(this Railway<T> value) =>
             HandleWithSuccessCode(201, value);
 
-        public static IActionResult MaybeNoContent<T>(this Railway<T> value) =>
+        public static async Task<IActionResult> Created<T>(this Task<Railway<T>> value) =>
+            HandleWithSuccessCode(201, await value);
+
+        public static IActionResult NoContent<T>(this Railway<T> value) =>
             HandleWithSuccessCode(204, value);
+
+        public static async Task<IActionResult> NoContent<T>(this Task<Railway<T>> value) =>
+            HandleWithSuccessCode(204, await value);
 
         private static IActionResult HandleWithSuccessCode<T>(int code, Railway<T> value) =>
             value.Handle(
